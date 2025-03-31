47a18b2b64c1e315eff0b0316f081bbd627c467e
Max <201191781+maxxeddd@users.noreply.github.com>
2025-03-31 21:06:42 +1000

    fix

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,19 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import SignUp from "../views/SignUp.vue";
 import SignIn from "../views/SignIn.vue";
 import Home from "@/views/Home.vue";
 import AboutUs from "@/views/AboutUs.vue";
 
-
 const routes = [
-  { path: "/", component: Home },
-  { path: "/sign-up", component: SignUp },
-  { path: "/about-us", component: AboutUs },
-  { path: "/sign-in", component: SignIn },
+    { path: "/", component: Home },
+    { path: "/sign-up", component: SignUp },
+    { path: "/about-us", component: AboutUs },
+    { path: "/sign-in", component: SignIn },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
